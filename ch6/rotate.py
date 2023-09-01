7bf0b49dfd81ec0249bd13ac9ff8e9ecf8f16e6c
Victor Joulin-Batejat <victorjb2015@gmail.com>
2023-09-01 09:03:39 -0700

    Basic chapter 6 image manipulations functions complete

--- ch6/rotate.py
+++ ch6/rotate.py
@@ -23,3 +23,7 @@ M = cv2.getRotationMatrix2D(center, -90, 1.0)
 rotated = cv2.warpAffine(image, M, (w, h))
 cv2.imshow("Rotated by -90 Degrees", rotated)
 cv2.waitKey(0)
+
+rotated = imutils.rotate(image, 180)
+cv2.imshow("Rotated by 180 Degrees", rotated)
+cv2.waitKey(0)
