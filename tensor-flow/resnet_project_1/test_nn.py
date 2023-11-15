a99748b89e72ffb5ba1528a86de7d18da52971cb
Victor Joulin-Batejat <victorjb2015@gmail.com>
2023-11-15 11:21:35 -0800

    Added test nn for practicing purposes, transfer learning complete

---
+++ tensor-flow/resnet_project_1/test_nn.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+
+def activation(num):
+    return ((num*.1) + .5)
+
+
+def return_nn_weights(m1, expected, w1=np.array([1, 1, 1])):
+    prediction = m1 @ w1
+    # print(prediction)
+
+    Ps = np.array(list(map(activation, prediction)))
+    print(Ps)
+
+    accuracy = [1 if p > 0.5 else 0 for p in Ps]
+    print(accuracy)
+
+
+    accuracy = sum([x for i, p in enumerate(accuracy) if p == expected[i]])
+
+data_m = np.array([[0, -1, 1],
+          [-1, -3, 1],
+          [2, -2, 1],
+          [3, 1, 1],
+          [1, -2, 1],
+          [-1, 1, 1],
+          [-2, -1, 1],
+          [0, 3, 1],
+          [-3, 0, 1],
+          [-1, 2, 1],
+         ])
+
+expected = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+
+epoch_num = 3
+
+# print("Initial data set:\n", data_m)
+
+return_nn_weights(data_m, expected)
--- tensor-flow/resnet_project_1/transfer_live.py
+++ tensor-flow/resnet_project_1/transfer_live.py
@@ -4,3 +4,61 @@ UCI Defungi dataset has been gitignored for sake of internet connection
 """
 
 import tensorflow as tf
+import tensorflow.keras.utils as utils
+import tensorflow.keras.applications.resnet50 as resnet50
+import tensorflow.keras as keras
+import tensorflow.keras.layers as layers
+import tensorflow.keras.optimizers as optimizers
+import tensorflow.keras.losses as losses
+
+train, validation = utils.image_dataset_from_directory(
+    'defungi',
+    label_mode = 'categorical',
+    image_size = (224, 224),
+    seed = 69420,
+    validation_split = 0.30,
+    subset = 'both',
+)
+
+# preprocessing input later from resnet50; just Xs because x is input, that's what
+# we need to preprocess
+train = train.map(lambda x, y: (resnet50.preprocess_input(x), y))
+validation = validation.map(lambda x, y: (resnet50.preprocess_input(x), y))
+
+print(train)
+print(validation)
+
+resnet = resnet50.ResNet50(
+    include_top = True,
+    weights = 'imagenet',
+    # classifier_activation = 'softmax',
+)
+
+print(f"{resnet=}")
+
+resnet.trainable = False
+
+inputs = keras.Input(shape = (224, 224, 3))
+
+outputs = resnet(inputs)
+outputs = layers.Dense(5, activation = 'softmax')(outputs)
+
+optimizer = optimizers.legacy.Adam(learning_rate = 0.00001)
+loss = losses.CategoricalCrossentropy()
+
+model = keras.Model(inputs, outputs)
+
+model.compile(
+    optimizer = optimizer,
+    loss = loss,
+    metrics = ['accuracy']
+)
+
+model.fit(
+    train,
+    batch_size = 32,
+    epochs = 10,
+    verbose = 1,
+    validation_data = validation,
+    validation_batch_size = 32
+)
