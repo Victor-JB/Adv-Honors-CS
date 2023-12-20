1696953363a7ed2754e211f140cd5bc55176713b
Victor Joulin - Batejat <victorjb2015@gmail.com>
2023-12-20 14:52:03 -0800

    minor

--- tensor-flow/transfer-learning-lab2/test_model.py
+++ tensor-flow/transfer-learning-lab2/test_model.py
@@ -5,6 +5,10 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--checkpoint_path", required = True,
                help = "Checkpoint path from which to load model")
+ap.add_argument("-i", "--test_image_path", required = False,
+               help = "Image path from which to test with one image")
+ap.add_argument("-d", "--test_dataset_path", required = False,
+               help = "Dataset path with test images")
 ap.add_argument("-e", "--eval_ds_path", required = False,
                help = "Path with dataset from which to evaluate the model")
 args = vars(ap.parse_args())
@@ -14,6 +18,9 @@ def main():
     model = tf.keras.models.load_model(args['checkpoint_path'])
     print(f"\nModel at '{args['checkpoint_path']}' loaded successfully\n")
     model.summary()
+
+    if args['test_image_path']:
+        
 
     if args['eval_ds_path']:
         print(f"\n\nEvaluating it with dataset at {args['eval_ds_path']}")
